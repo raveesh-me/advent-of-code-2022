4cbedf81c118007b1fdabde58d7cc445f102a5fa
Raveesh Agarwal <raveeshr503@gmail.com>
2022-12-06 10:53:29 +0530

    add: solve day06 part 1 and 2

--- day-06/solution.dart
+++ day-06/solution.dart
@@ -4,7 +4,20 @@ String kInputFilePath = "day-06/input.txt";
 
 int main(List<String> args) {
   File inputFile = File.fromUri(Uri(path: kInputFilePath));
-  List<String> lines = inputFile.readAsLinesSync();
-  lines.forEach(print);
+  String input = inputFile.readAsLinesSync()[0];
+  for (var i = 3; i < input.length; i++) {
+    if ({input[i], input[i - 1], input[i - 2], input[i - 3]}.length == 4) {
+      print("start-of-packet: ${i + 1}");
+      break;
+    }
+  }
+
+  for (var i = 13; i < input.length; i++) {
+    if (Set.from(List.generate(14, (index) => input[i - index])).length ==
+        14) {
+      print("start-of-message: ${i + 1}");
+      break;
+    }
+  }
   return 0;
 }
